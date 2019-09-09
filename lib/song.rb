483b00cab54fe6c62fcd41107ffb451c8377cc0f
Indu Ramesh <induramesh@gmail.com>
2019-09-09 02:32:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,6 +51,10 @@ class Song
   def self.create_from_filename(filename)
     self.new_from_filename(filename).save
   end 
+  
+  def destroy_all
+    self.all.clear
+  end 
     
   
   def self.all
