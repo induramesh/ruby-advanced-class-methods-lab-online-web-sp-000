a34cea07c99a9fd27f931b9416a9043de5e064da
Indu Ramesh <induramesh@gmail.com>
2019-09-09 00:49:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,18 @@
 class Song
+  
+  def initialize(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+  end 
+  
   attr_accessor :name, :artist_name
+  
   @@all = []
+  
+  def create
+    save
+    self 
+  end 
 
   def self.all
     @@all
