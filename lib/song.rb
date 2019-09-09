acf070b85e786a1a8e80ca0c255ae2a9f5f69af3
Indu Ramesh <induramesh@gmail.com>
2019-09-09 02:04:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,10 +28,11 @@ class Song
   end 
   
   def self.find_or_create_by_name(song_name)
-    if self.find_by_name(song_name) == false 
+     res = self.find_by_name(song_name) 
+     if res == nil 
       self.create_by_name(song_name)
     else
-      self.find_by_name(song_name) 
+      res 
     end 
   end 
 
